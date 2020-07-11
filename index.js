3430ddbc0b76dc61812407d8d9dbec3f8c00104a
mcbryan1 <git config --global user.email solomonmcbryan@gmail.comgit config --global user.email>
2020-07-11 13:55:21 +0000

    Create Model Working Successfully

--- index.js
+++ index.js
@@ -3,50 +3,51 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const server = express();
 
-
 //Middleware
 server.use(bodyParser.json());
 
-
 //Database
-const bankDB = [
-
-];
-
+const bankDB = [];
 
 //Models
-class BankModel{
-    constructor({name, branch, location, phone, address, accountNumber}){
-        this.name = name;
-        this.branch = branch;
-        this.location = location,
-        this.phone = phone,
-        this.address = address,
-        this.accountNumber = accountNumber
-    }
+class BankModel {
+  constructor({ name, branch, location, phone, address, accountNumber }) {
+    this.name = name;
+    this.branch = branch;
+    (this.location = location),
+      (this.phone = phone),
+      (this.address = address),
+      (this.accountNumber = accountNumber);
+  }
+  save() {
+    bankDB.push(this);
+    return this;
+  }
 }
-
 
 //Controllers
-const listBanksController =(req, res)=>{
-
-}
-const createBankController =(req, res)=>{
-
-}
-const updateBankController =(req, res)=>{
-
-}
-const deleteBankController =(req, res)=>{
-
-}
+const listBanksController = (req, res) => {};
+const createBankController = (req, res) => {
+  const { name, branch, location, phone, address, accountNumber } = req.body;
+  const bank = new BankModel({
+    name,
+    branch,
+    location,
+    phone,
+    address,
+    accountNumber,
+  });
+  bank.save();
+  res.json({message: "Create Successful", data: bank})
+};
+const updateBankController = (req, res) => {};
+const deleteBankController = (req, res) => {};
 
 //Routes
-server.get('/bank', listBanksController)
-server.post('/post', createBankController)
-server.put('/bank', updateBankController)
-server.delete('/bank', deleteBankController)
-
+server.get("/bank", listBanksController);
+server.post("/bank", createBankController);
+server.put("/bank", updateBankController);
+server.delete("/bank", deleteBankController);
 
 //Listening To Server
 server.listen(3000, () => console.log("Server Is Ready For Any Request"));
